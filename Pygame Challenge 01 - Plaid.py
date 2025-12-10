a102c968d1b989d6f9e09bcb4276236b0bd8101c
DavidBlits <blitsdavid@gmail.com>
2025-12-09 22:43:06 -0500

    Finished Lab 1

--- Pygame Challenge 01 - Plaid.py
+++ Pygame Challenge 01 - Plaid.py
@@ -31,4 +31,27 @@
 # Good luck!
 #-----------------------------------------------------------------------------
 
-
+import pygame
+pygame.init()
+Width,Height = 800,800
+window = pygame.display.set_mode((Width,Height))
+pygame.display.set_caption("Plaid pattern")
+WHITE = (255, 255, 255)
+RED   = (255, 0, 0)
+TRANSPARENT_BLUE =(0, 0, 255, 100)
+overlay = pygame.Surface((Width, Height), pygame.SRCALPHA)
+window.fill(WHITE)
+for x in range(0,Width,100):
+    pygame.draw.line(window, RED, (x,0),(x,Height),20)
+    pygame.draw.line(window, RED, (x + 30, 0),(x + 30, Height),5)
+for y in range(0,Height,100):
+    pygame.draw.line(overlay, TRANSPARENT_BLUE, (0, y),(Width, y    ),20)
+    pygame.draw.line(overlay, TRANSPARENT_BLUE, (0, y+30),(Width, y+30),5)
+window.blit(overlay, (0, 0))
+pygame.display.flip()
+exit= True
+while exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = False
+pygame.quit()
