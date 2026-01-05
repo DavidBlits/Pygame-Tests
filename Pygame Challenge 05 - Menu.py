6286fd220033f57e8989e32c3b40f458bfc57b26
DavidBlits <blitsdavid@gmail.com>
2026-01-05 10:05:36 -0500

    Lab 5 done

--- Pygame Challenge 05 - Menu.py
+++ Pygame Challenge 05 - Menu.py
@@ -46,4 +46,75 @@
 # Good luck!
 #-----------------------------------------------------------------------------
 
+import pygame
+import sys
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Gamestate Lab")
+font = pygame.font.SysFont("Arial", 30)
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+LIGHT_GRAY = (200, 200, 200)
+RED = (255, 100, 100)
+GREEN = (100, 255, 100)
+BLUE = (100, 100, 255)
+
+state = "MENU"
+
+def draw_button(text, x, y, w, h, action_if_clicked):
+    global state
+    mouse_pos = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()[0]
+    rect = pygame.Rect(x, y, w, h)
+    color = LIGHT_GRAY if rect.collidepoint(mouse_pos) else GRAY
+    
+    pygame.draw.rect(screen, color, rect)
+    pygame.draw.rect(screen, BLACK, rect, 3) 
+    
+    text = font.render(text, True, BLACK)
+    text_rect = text.get_rect(center=rect.center)
+    screen.blit(text, text_rect)
+    if rect.collidepoint(mouse_pos) and click:
+        state = action_if_clicked
+        pygame.time.delay(150)
+# MAIN LOOP 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if state == "MENU":
+        screen.fill(WHITE)
+        title1 = pygame.font.Font('fonts/NotoSansJP-VariableFont_wght.ttf',30)
+        title = title1.render("MAIN MENU", True, BLACK)
+        screen.blit(title, (320, 50))
+        draw_button("Go to Plaid", 300, 150, 200, 50, "PLAID")
+        draw_button("Go to Stripes", 300, 220, 200, 50, "STRIPES")
+        draw_button("Go to Solids", 300, 290, 200, 50, "SOLIDS")
+    elif state == "PLAID":
+        screen.fill(WHITE)
+        pygame.draw.rect(screen, RED, (0, 0, 100, HEIGHT))
+        pygame.draw.rect(screen, RED, (200, 0, 100, HEIGHT))
+        pygame.draw.rect(screen, RED, (400, 0, 100, HEIGHT))
+        pygame.draw.rect(screen, RED, (600, 0, 100, HEIGHT))
+        pygame.draw.rect(screen, BLUE, (0, 100, WIDTH, 50))
+        pygame.draw.rect(screen, BLUE, (0, 300, WIDTH, 50))
+        pygame.draw.rect(screen, BLUE, (0, 500, WIDTH, 50))
+        draw_button("Back to Menu", 50, 500, 200, 50, "MENU")
+    elif state == "STRIPES":
+        screen.fill(WHITE)
+        for i in range(0, WIDTH, 40):
+            pygame.draw.line(screen, GREEN, (i, 0), (i, HEIGHT), 5)
+            
+        draw_button("Back to Menu", 50, 500, 200, 50, "MENU")
+
+    elif state == "SOLIDS":
+        screen.fill(BLUE)
+        draw_button("Back to Menu", 50, 500, 200, 50, "MENU")
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
